bc9d88a5a0b4f186c7f62b0c3a48e5ac682b2bde
akinwilson <akinola.antony.wilson@gmail.com>
2022-02-25 14:30:13 +0000

    vpc endpoint

--- iac/vpc/main.tf
+++ iac/vpc/main.tf
@@ -8,7 +8,14 @@ resource "aws_vpc" "main" {
     }
 }
 
-
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.${var.region}.s3"
+    tags {
+        Name = "${var.name}-vpc-endpoint-s3-${var.environment}"
+        Environment = var.environment
+    }
+}
 
 resource "aws_subnet" "isolated" {
   vpc_id = aws_vpc.main.id
