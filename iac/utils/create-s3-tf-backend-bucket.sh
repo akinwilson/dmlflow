70a9776effb89230e3f4fe84574e72cd45355a15
akinwilson <akinola.antony.wilson@gmail.com>
2022-02-25 12:31:34 +0000

    adjustments to initial bucket creationd and deletion scripts
    Need to accept command line inputs to override the aws info  in both .sh files
    need to reorder flow for deletion of state store buckets.
    in /delete-s3-tf-backend-bucket.sh, Try to force remove bucket, if fails, then remove objects first
    and retry force removing. Atm, first rm objects (fails if there isnt any) then rm bucket. need if else statement

---
+++ iac/utils/create-s3-tf-backend-bucket.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+# first need the bucket to exists in aws where the  state of the infrastructure is kept
+
+AWS_PROFILE="akinwilson"
+AWS_REGION="eu-west-2"
+AWS_BUCKET="infra-euw2"
+
+echo "Creating backend s3 bucket for terraform version control.."
+echo "This bucket will track the version of infrastructure deployed"
+echo "aws account: ${AWS_PROFILE}"
+echo "aws region: ${AWS_REGION}"
+echo "aws s3 bucket name: ${AWS_BUCKET}"
+
+aws s3api create-bucket \
+--bucket $AWS_BUCKET \
+--acl private \
+--region $AWS_REGION \
+--create-bucket-configuration '{"LocationConstraint":"'"$AWS_REGION"'"}' \
+--profile $AWS_PROFILE >/dev/null 
+
+aws s3api put-bucket-tagging \
+--bucket $AWS_BUCKET \
+--tagging 'TagSet=[{Key=purpose,Value=tf-store}]' \
+--profile $AWS_PROFILE >/dev/null
+
+aws s3api put-bucket-versioning \
+--bucket $AWS_BUCKET \
+--versioning-configuration Status=Enabled \
+--profile $AWS_PROFILE >/dev/null
+
+exit 1
---
+++ iac/utils/delete-s3-tf-backend-bucket.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+set -e
+
+AWS_PROFILE="akinwilson"
+AWS_REGION="eu-west-2"
+AWS_BUCKET="infra-euw2"
+echo "Delete state store bucket of terraform"
+echo "aws account: ${AWS_PROFILE}"
+echo "aws region: ${AWS_REGION}"
+echo "aws s3 bucket name: ${AWS_BUCKET}"
+
+# Delete objects inside state-store bucket 
+# NOTE: try force remove first, if fails, then remove objects first 
+# and retry force removing.
+
+aws s3api delete-objects --bucket $AWS_BUCKET --delete "$(aws s3api list-object-versions --bucket infra-euw2 --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}')" > /dev/null
+
+# Force remove bucket 
+aws s3 rb s3://$AWS_BUCKET --force
+
+
+echo "Finished clean"
+exit 1
+
+
