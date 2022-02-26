b8423aa7bab981ce670f5fd39e617a014005ef15
akinwilson <akinola.antony.wilson@gmail.com>
2022-02-26 13:05:45 +0000

    further work on variable inputs for main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -54,15 +54,22 @@ module "alb" {
     source = "./alb"
     name = var.name
     environment = var.environment
-    public_subnets = vpc.public
-    vpc_id = vpc.id 
-    sg = sg.alb 
+    public_subnets = module.vpc.public
+    vpc_id = module.vpc.id 
+    sg = module.sg.alb 
 }
 
 module ecs {
     source = "./ecs"
     name = var.name
+    region = var.region
+    sg = module.sg.ecs
     environment = var.environment
+    ecr_repo_url = module.ecr.ecr_repo_url
+    private_subnets = module.vpc.private
+    container_port = var.container_port
+    container_cpu = var.container_cpu
+    container_memory = var.container_memory
 }
 
 module "ecr" {
@@ -70,4 +77,98 @@ module "ecr" {
     name = var.name
     environment = var.environment
 
+}
+
+
+
+
+
+
+data "aws_iam_policy_document" "iam_policy_document" {
+  statement {
+    sid     = "AllowSpecificS3FullAccess"
+    actions = ["s3:*"]
+    effect  = "Allow"
+    resources = [
+      "arn:aws:s3:::*/*",
+      "arn:aws:s3:::*",
+      "arn:aws:s3:::infra-euw2",
+      "arn:aws:s3:::infra-euw2",
+    ]
+  }
+
+  statement {
+    sid = "AllowSecurityGroups"
+    actions = [
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSecurityGroupsRules",
+      "ec2:DescribeTags",
+      "ec2:CreateTags",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:RevokeSecurityGroupIngress",
+      "ec2:AuthorizeSecurityGroupEgress",
+      "ec2:ModifySecurityGroupEgress",
+      "ec2:ModifySecurityGroupRuleDescriptionIngress",
+      "ec2:ModifySecurityGroupRuleDescriptionEgress",
+      "ec2:ModifySecurityGroupRules",
+      "ec2:CreateSecurityGroup"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+  statement {
+    sid = "AllowEC2"
+    actions = [
+      "ec2:*"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+  statement {
+    sid = "AllowIAM"
+    actions = [
+      "iam:*"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+  statement {
+    sid = "AllowSecretsManager"
+    actions = [
+      "secretsmanager:*"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+  statement {
+    sid = "AllowSSM"
+    actions = [
+      "ssm:PutParameter",
+      "ssm:DeleteParameter",
+      "ssm:GetParameterHistory",
+      "ssm:GetParametersByPath",
+      "ssm:GetParameters",
+      "ssm:GetParameter",
+      "ssm:DeleteParameters",
+      "ssm:DescribeParameters",
+      "ssm:AddTagsToResource",
+      "ssm:ListTagsForResource"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "iam_policy" {
+  name   = "terraform-iam-policy"
+  path   = "/"
+  policy = data.aws_iam_policy_document.iam_policy_document.json
+}
+
+resource "aws_iam_user" "terraform_agent_user" {
+  name = "terraform_agent_user"
+}
+resource "aws_iam_user_policy_attachment" "tf_attach" {
+  user       = aws_iam_user.terraform_agent_user.name
+  policy_arn = aws_iam_policy.iam_policy.arn
 }
