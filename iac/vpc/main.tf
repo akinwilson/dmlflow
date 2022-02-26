ac2859733614f0eac686a28f7b797ae9a6a8ab10
akinwilson <akinola.antony.wilson@gmail.com>
2022-02-26 12:30:11 +0000

    logging for vpc

--- iac/vpc/main.tf
+++ iac/vpc/main.tf
@@ -153,3 +153,53 @@ resource "aws_route_table_association" "isolated" {
   route_table_id = element(aws_route_table.isolated.*.id, count.index)
 }
 
+resource "aws_flow_log" "main" {
+  iam_role_arn    = aws_iam_role.vpc-flow-logs-role.arn
+  log_destination = aws_cloudwatch_log_group.main.arn
+  traffic_type    = "ALL"
+  vpc_id          = aws_vpc.main.id
+}
+
+resource "aws_cloudwatch_log_group" "main" {
+  name = "${var.name}-cloudwatch-log-group"
+}
+
+resource "aws_iam_role" "vpc-flow-logs-role" {
+  name = "${var.name}-vpc-flow-logs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Sid    = "",
+      Effect = "Allow",
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "vpc-flow-logs.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "vpc-flow-logs-policy" {
+  name = "${var.name}-vpc-flow-logs-policy"
+  role = aws_iam_role.vpc-flow-logs-role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogGroups",
+          "logs:DescribeLogStreams"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+  ] })
+}
+
+
+
