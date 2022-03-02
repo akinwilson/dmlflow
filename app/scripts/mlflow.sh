296db903822f6add6c3dc6e6318ae5d4e57c8ad0
akinwilson <akinola.antony.wilson@gmail.com>
2022-03-02 11:00:31 +0000

    executable to init mlflow server with db credentials. Need to pass from tf into script

---
+++ app/scripts/mlflow.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -x 
+
+
+if [[ -z "${MLFLOW_ARTIFACT_URI}" ]]; then
+    echo "MLFLOW_ARTIFACT_URI can not be set. Define default value as ./mlruns"
+    export MLFLOW_ARTIFACT_URI="./mlruns"
+fi
+
+if [[ -z "${MLFLOW_ARTIFACT_DESTINATION}"]]; then
+    echo "MLFLOW_ARTIFACT_DESTINATION has not been set"
+    export "MLFLOW_ARTIFACT_DESTINATION"="s3://"
+
+if [[ -z "${MLFLOW_DB_DIALECT}" ]]; then
+    export MLFLOW_DB_DIALECT="mysql+pymysql"
+fi
+
+if [[ -z "${MLFLOW_DB_USERNAME}" ]]; then
+    export MLFLOW_DB_USERNAME="mlflow"
+fi
+
+if [[ -z "${MLFLOW_DB_PASSWORD}" ]]; then
+    export MLFLOW_DB_PASSWORD="mlflow"
+fi
+
+if [[ -z "${MLFLOW_DB_DATABASE}" ]]; then
+    export MLFLOW_DB_DATABASE="mlflow"
+fi
+
+if [[ -z "${MLFLOW_DB_PORT}" ]]; then
+    export MLFLOW_DB_PORT=3306
+fi
+
+if [[ -z "${MLFLOW_BACKEND_URI}" ]]; then
+    echo "MLFLOW_BACKEND_URI not set. Define default value based on other variables."
+    export MLFLOW_BACKEND_URI=${MLFLOW_DB_DIALECT}://${MLFLOW_DB_USERNAME}:${MLFLOW_DB_PASSWORD}@${MLFLOW_DB_HOST}:${MLFLOW_DB_PORT}/${MLFLOW_DB_DATABASE}
+fi
+
+exec mlflow server \
+    --host 0.0.0.0 \
+    --port 5001 \
+    --default-artifact-root "${MLFLOW_ARTIFACT_URI}" \
+    --artifacts-destination "${MLFLOW_ARTIFACT_DESTINATION}" \
+    --backend-store-uri "${MLFLOW_BACKEND_URI}"
