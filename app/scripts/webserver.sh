22aa247005939b286cfb8ec90ebed69bc1136cd9
akinwilson <akinola.antony.wilson@gmail.com>
2022-03-02 11:01:55 +0000

    webserver init exectuable

--- app/scripts/webserver.sh
+++ app/scripts/webserver.sh
@@ -1,1 +1,14 @@
 
+#!/bin/bash
+set -x
+
+if [[ -z "${MLFLOW_TRACKING_USERNAME}" ]]; then
+    export MLFLOW_TRACKING_USERNAME="mlflow"
+fi
+
+if [[ -z "${MLFLOW_TRACKING_PASSWORD}" ]]; then
+    export MLFLOW_TRACKING_PASSWORD="mlflow"
+fi
+
+htpasswd -b -c /etc/nginx/.htpasswd ${MLFLOW_TRACKING_USERNAME} ${MLFLOW_TRACKING_PASSWORD}
+exec nginx -g "daemon off;"
