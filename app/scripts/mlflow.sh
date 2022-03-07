283ede08e1ded099eb070981ce182777e0b467e0
akinwilson <akinola.antony.wilson@gmail.com>
2022-03-07 08:44:53 +0000

    new dockerfile with authentication using nginx

--- app/scripts/mlflow.sh
+++ app/scripts/mlflow.sh
@@ -2,57 +2,20 @@
 set -x 
 echo "Initialising Mlflow tracking server..."
 echo ""
-# NOTE!
-# These env vars are being passed to the ecr task and into the serving container 
-# Need to configure container to require these at runtime.
-# that is: 
-#        DB_ENV_VARS 
-#        BUCKET_ID/NAME 
-# need to be passed to this script, from the dockerfile. 
-
-# -----> Solution:
-#        build the dockerfile with push-to-ecr via local-provisioner, but pass vars 
-#        building of the docker container 
-# envsubst < db-s3-config.txt  
 
 if [[ -z "${MLFLOW_ARTIFACT_URI}" ]]; then
     echo "MLFLOW_ARTIFACT_URI can not be set. Define default value as ./mlruns"
     export MLFLOW_ARTIFACT_URI="./mlruns"
 fi
 
-if [[ -z "${MLFLOW_ARTIFACT_DESTINATION}" ]]; then
-    echo "MLFLOW_ARTIFACT_DESTINATION has not been set. Defaulting to s3"
-    export MLFLOW_ARTIFACT_DESTINATION="s3"
-fi 
-
-if [[ -z "${MLFLOW_DB_DIALECT}" ]]; then
-    export MLFLOW_DB_DIALECT="mysql+pymysql"
-fi
-
-if [[ -z "${MLFLOW_DB_USERNAME}" ]]; then
-    export MLFLOW_DB_USERNAME="mlflow"
-fi
-
-if [[ -z "${MLFLOW_DB_PASSWORD}" ]]; then
-    export MLFLOW_DB_PASSWORD="mlflow"
-fi
-
-if [[ -z "${MLFLOW_DB_DATABASE}" ]]; then
-    export MLFLOW_DB_DATABASE="mlflow"
-fi
-
-if [[ -z "${MLFLOW_DB_PORT}" ]]; then
-    export MLFLOW_DB_PORT=3306
-fi
-
 if [[ -z "${MLFLOW_BACKEND_URI}" ]]; then
     echo "MLFLOW_BACKEND_URI not set. Define default value based on other variables."
-    export MLFLOW_BACKEND_URI=${MLFLOW_DB_DIALECT}://${MLFLOW_DB_USERNAME}:${MLFLOW_DB_PASSWORD}@${MLFLOW_DB_HOST}:${MLFLOW_DB_PORT}/${MLFLOW_DB_DATABASE}
 fi
 
 exec mlflow server \
     --host 0.0.0.0 \
     --port 5001 \
     --serve-artifacts \
+    --default-artifact-root "mlflow-artifacts:/"
     --artifacts-destination "${MLFLOW_ARTIFACT_URI}" \
     --backend-store-uri "${MLFLOW_BACKEND_URI}"
