a7bbe9aeae21eb522e1fa3247797bad6e684f954
akinwilson <akinola.antony.wilson@gmail.com>
2022-04-16 17:07:28 +0100

    private bucket

--- iac/s3/main.tf
+++ iac/s3/main.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "main" {
   bucket        = var.bucket_name
-  acl           = "public-read-write"
+  acl           = "private"
   force_destroy = true
-  # tags = {
-  #   Name        = "${var.name}-artifacts-bucket-${var.environment}"
-  #   Environment = var.environment
-  # }
+  tags = {
+    Name        = "${var.name}-artifacts-bucket-${var.environment}"
+    Environment = var.environment
+  }
 }
 
 output "bucket" {
