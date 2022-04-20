756daac4bce1e26f8892d7e1aed4018a7ab8767b
akinwilson <akinola.antony.wilson@gmail.com>
2022-04-20 12:47:03 +0100

    added helper loader function inside training script

--- model/train.py
+++ model/train.py
@@ -5,6 +5,8 @@ from argparse import ArgumentParser
 
 import pandas as pd 
 import numpy as np 
+import pickle 
+
 
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score 
 from sklearn.model_selection import train_test_split 
@@ -20,6 +22,9 @@ def eval_metrics(y,y_hat):
     r2 = r2_score(y, y_hat) 
     return rmse, mae, r2
 
+def load_data():
+    return pickle.load(open("data.p", "rb"))
+
 
 if __name__ == "__main__":
     # setting seed for reproducibility 
@@ -27,8 +32,6 @@ if __name__ == "__main__":
     print("imports okay")
     parser = ArgumentParser()
     parser.add_argument("--alpha")
-    parser.add_argument("l1_ratio")
+    parser.add_argument("--l1_ratio")
 
     args = parser.parse_args()
-
-    
