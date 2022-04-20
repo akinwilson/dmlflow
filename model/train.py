bae4ea99179616f5f52954db65e64dc5b0a26d08
akinwilson <akinola.antony.wilson@gmail.com>
2022-04-20 14:03:48 +0100

    inferring signature and example input

--- model/train.py
+++ model/train.py
@@ -7,12 +7,11 @@ from argparse import ArgumentParser
 import pandas as pd 
 import numpy as np 
 import pickle 
-
-
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score 
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import ElasticNet 
 
+from mlflow.models.signature import infer_signature
 import mlflow
 import mlflow.sklearn 
 
@@ -25,7 +24,12 @@ def eval_metrics(y,y_hat):
     return rmse, mae, r2
 
 def load_data():
+    '''load the pickled dataset'''
     return pickle.load(open("data.p", "rb"))
+
+def get_model_signature(x, y_hat):
+    '''inputs are expected to be a dataframe and the prediction of the model'''
+    return infer_signature(x, y_hat)
 
 
 if __name__ == "__main__":
@@ -55,9 +59,29 @@ if __name__ == "__main__":
     alpha = float(args.alpha)
     l1_ratio = float(args.l1_ratio)
 
-
     with mlflow.start_run():
         lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_seed=42)
         lr.fit(train_x,train_y)
-
+        
         y_hat = lr.predict(test_x)
+
+        rmse, mae, r2 = eval_metrics(test_y, y_hat)
+        
+        print(f"ElasticNet regressor model (alpha={alpha}, l1_ratio={l1_ratio})")
+        print(f"RMSE: {rmse}")
+        print(f"MAE: {mae}")
+        print(f"R2: {r2}")
+        # model parameters logging 
+        mlflow.log_params("alpha", alpha)
+        mlflow.log_params("l1_ratio", l1_ratio)
+        # evaluation metric logging 
+        mlflow.log_metric("rmse",rmse)
+        mlflow.log_metric("mae",mae)
+        mlflow.log_metric("r2",r2)
+
+        model_sig = get_model_signature(test_x, y_hat)
+        mlflow.sklearn.log_model(sk_model=lr,
+                                artifact_path="model",
+                                registered_model_name="ElasticNetRegressor",
+                                signature=model_sig,
+                                input_example=test_x.head(1))
