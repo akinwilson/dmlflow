7924ae17c4095a67a1a48bd0d9ab916da78c6c3b
akinwilson <akinola.antony.wilson@gmail.com>
2022-04-20 13:20:09 +0100

    container training example

--- model/train.py
+++ model/train.py
@@ -1,4 +1,5 @@
-import os, sys 
+import os, sys
+from random import random 
 import warnings 
 
 from argparse import ArgumentParser
@@ -17,6 +18,7 @@ import mlflow.sklearn
 
 
 def eval_metrics(y,y_hat):
+    '''metrics for evaluation'''
     rmse = np.sqrt(mean_squared_error(y,y_hat))
     mae = mean_absolute_error(y,y_hat)
     r2 = r2_score(y, y_hat) 
@@ -30,8 +32,32 @@ if __name__ == "__main__":
     # setting seed for reproducibility 
 
     print("imports okay")
+    
     parser = ArgumentParser()
     parser.add_argument("--alpha")
     parser.add_argument("--l1_ratio")
 
     args = parser.parse_args()
+    
+    data = load_data()
+
+    train, test = train_test_split(data)
+
+    # features 
+    train_x = train.drop(["quality"], axis=1)
+    test_x = test.drop(["quality"], axis=1)
+
+    # targets 
+    train_y = train[["quality"]]
+    test_y = test[["quality"]]
+
+    # parameters 
+    alpha = float(args.alpha)
+    l1_ratio = float(args.l1_ratio)
+
+
+    with mlflow.start_run():
+        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_seed=42)
+        lr.fit(train_x,train_y)
+
+        y_hat = lr.predict(test_x)
